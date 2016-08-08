b7802b8ff9ae849b187cf7e9d1bba4ebb4da959b
antonparas <anton.paras@icloud.com>
2016-08-08 09:24:08 -0700

    Enable ChatConnector by default
    Made ChatConnector the default by uncommenting it.
    
    I did this mainly because I'm testing the connection to Azure right now.
    ChatConnector must be enabled to connect to Azure.

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var builder = require('botbuilder');
  *    - ChatConnector, for online/deployment testing.
  *
  * UNCOMMENT THE CONNECTOR YOU WISH TO USE.
+ * ~CHAT CONNECTOR IS ENABLED BY DEFAULT~
  */
 
 // CONSOLE CONNECTOR
@@ -21,23 +22,23 @@ var builder = require('botbuilder');
 
 
 // CHAT CONNECTOR
-// var connector = new builder.ChatConnector({
-//   appId: process.env.MICROSOFT_APP_ID,
-//   appPassword: process.env.MICROSOFT_APP_PASSWORD
-// });
-//
-// var server = restify.createServer();
-//
-// server.post('/api/messages', connector.listen());
-//
-// server.get(/.*/, restify.serveStatic({
-//   'directory': '.',
-//   'default': 'index.html'
-// }));
-//
-// server.listen(process.env.port || 3978, function () {
-//   console.log('%s listening to %s', server.name, server.url);
-// });
+var connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+var server = restify.createServer();
+
+server.post('/api/messages', connector.listen());
+
+server.get(/.*/, restify.serveStatic({
+  'directory': '.',
+  'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
+});
 
 
 
